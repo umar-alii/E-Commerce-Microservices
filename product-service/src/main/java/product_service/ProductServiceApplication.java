4a949c9b9f2b93a8c5ba4875f04c860ba8e88178
umar-alii <umarrali.pk@gmail.com>
2026-04-26 17:20:48 +0500

    addition of swagger and basic auth at gateway level

--- product-service/src/main/java/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/product_service/ProductServiceApplication.java
@@ -3,9 +3,12 @@ package product_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "API Gateway")})
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/user_service/UserServiceApplication.java
+++ user-service/src/main/java/user_service/UserServiceApplication.java
@@ -2,8 +2,11 @@ package user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "API Gateway")})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
